336c938f4a2fd1d952ff53b549f0f64009b5ff9d
dimnov <dimnov1512@gmail.com>
2024-04-15 20:13:31 +0300

    Create Friend.jsx

---
+++ src/components/Friend.jsx
@@ -0,0 +1,24 @@
+function Friend({ friend }) {
+ return (
+  <li>
+   <img src={friend.image} alt={friend.name} />
+   <h3>{friend.name}</h3>
+
+   {friend.balance < 0 && (
+    <p className="red">
+     You owe {friend.name} {Math.abs(friend.balance)}€
+    </p>
+   )}
+
+   {friend.balance > 0 && (
+    <p className="green">
+     You owe {friend.name} {Math.abs(friend.balance)}€
+    </p>
+   )}
+
+   {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+  </li>
+ );
+}
+
+export default Friend;
