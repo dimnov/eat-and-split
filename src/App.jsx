270b1a433b1c8a5b8656cb7384b0f68e6cde39f0
dimnov <dimnov1512@gmail.com>
2024-04-18 17:23:52 +0300

    Add splitting the bill functionality

--- src/App.jsx
+++ src/App.jsx
@@ -50,6 +50,18 @@ function App() {
   setShowAddFriend(false);
  }
 
+ function handleSplitBill(value) {
+  setFriends((friends) =>
+   friends.map((friend) =>
+    friend.id === selectedFriend.id
+     ? { ...friend, balance: friend.balance + value }
+     : friend
+   )
+  );
+
+  setSelectedFriend(null);
+ }
+
  return (
   <div className="app">
    <div className="sidebar">
@@ -69,7 +81,9 @@ function App() {
     </Button>
    </div>
 
-   {selectedFriend && <FormSplitBill name={selectedFriend.name} />}
+   {selectedFriend && (
+    <FormSplitBill name={selectedFriend.name} onSplitBill={handleSplitBill} />
+   )}
   </div>
  );
 }
